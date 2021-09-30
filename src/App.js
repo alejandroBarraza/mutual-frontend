63c8cba9807cc519f2dce1de418869efb81fa064
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-09-30 17:58:56 -0300

    fixing import routes and general style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './App.css';
-import { AppRouter } from './components/routers/AppRouter';
+import { AppRouter } from './routers/AppRouter';
 
 const client = new ApolloClient({
     uri: 'http://localhost:1337/graphql',
