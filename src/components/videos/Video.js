a31b17483e71523bf3a40408b97c4ffaff0dc39b
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-10-04 16:02:06 -0300

    refactor code, now using query components

--- src/components/videos/Video.js
+++ src/components/videos/Video.js
@@ -4,20 +4,20 @@ import { YoutubeEmbed } from './YoutubeEmbed';
 import { GETVIDEOS } from '../../Graphql/Queries';
 import './Video.css';
 import '../../container.css';
-import { Loading } from '../../helpers/Loading';
+import { Query } from '../utils/Query';
 
 export const Video = () => {
-    const { data, loading, error } = useQuery(GETVIDEOS);
-    if (loading) return <Loading />;
-    if (error) return <p>{JSON.stringify(error)}</p>;
-
     return (
-        <div className='container'>
-            <div>
-                <h2 className='title-video'>{data.videos[0].titulo}</h2>
-                <p className='description-video'>{data.videos[0].descripcion}</p>
-                <YoutubeEmbed embedId={data.videos[0].links} />
-            </div>
-        </div>
+        <Query query={GETVIDEOS} id={null}>
+            {({ data: { videos } }) => {
+                return (
+                    <div className='container'>
+                        <h2 className='title-video'>{videos[0].titulo}</h2>
+                        <p className='description-video'>{videos[0].descripcion}</p>
+                        <YoutubeEmbed embedId={videos[0].links} />
+                    </div>
+                );
+            }}
+        </Query>
     );
 };
