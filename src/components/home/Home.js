a0ded8b14696293c6e37ff6fc6905d1583436e4c
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2021-10-04 16:31:17 -0300

    fix the navbar, mov to the correct file

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Video } from '../videos/Video';
-import { Navbar } from './navbar/Navbar';
 
 export const Home = () => {
     return (
         <>
-            <Navbar />
             <Video />
         </>
     );
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from '../components/home/Home';
+import { Navbar } from '../components/home/navbar/Navbar';
 import { News } from '../components/news/News';
 
 export const AppRouter = () => {
     return (
         <Router>
+            <Navbar />
             <Switch>
                 <Route exact path='/noticias' component={News} />
                 <Route path='/' component={Home} />
