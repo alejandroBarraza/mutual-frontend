3ac3325037e2f527277d0453f5a6a371e1fe6013
Jorge Oyaneder <jorge.oyaneder.velasquez@gmail.com>
2021-10-06 17:38:11 -0300

    Enzyme configuration to perform Test.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,20 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// import '@testing-library/jest-dom';
+
+// Enzyme imports
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+//Enzyme-to-json imports
+import { createSerializer } from 'enzyme-to-json';
+
+// EnEnzyme configuration
+Enzyme.configure({ adapter: new Adapter() });
+//Enzyme-to-json configuration
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+/**
+ * ***INICIAR PRUEBAS***
+ * To start the tests we must go to the frontend folder and execute the command 'npm run test'.
+ */
