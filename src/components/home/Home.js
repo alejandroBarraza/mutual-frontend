5247816f259123bda4ea319d24ed2fad46fccc4a
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-10-06 12:39:16 -0300

    statistics component responsice

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Statistics } from '../statistics/Statistics';
 import { Video } from '../videos/Video';
 
 export const Home = () => {
     return (
         <>
             <Video />
+            <Statistics />
         </>
     );
 };
---
+++ src/components/statistics/StatisticCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const StatisticCard = () => {
+    return (
+        <div className='card'>
+            <h1>hola</h1>
+            <h1>86%</h1>
+            <p>satistaccion a la atencion</p>
+        </div>
+    );
+};
