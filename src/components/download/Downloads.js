84d0eba2508a54e6dfba55f68c814d2f73734915
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-10-20 17:49:02 -0300

    styled-component

--- src/components/download/Downloads.js
+++ src/components/download/Downloads.js
@@ -1,9 +1,95 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
+// import { Button } from '../utils/Button';
+import styled from 'styled-components';
+import { Button } from '../utils/Button';
 
 export const Downloads = () => {
+    // const cardContainerRef = useRef(null);
+
+    const CardContainer = styled.div`
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+        background-color: red;
+        margin: 1rem;
+        border-radius: 20px;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+        margin: 1rem;
+
+        transition: box-shadow 0.2s ease;
+        &:hover {
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        }
+    `;
+    const ImageContainer = styled.div`
+        flex: 0 1 50%;
+        img {
+            /* height: auto;
+            width: auto; */
+            min-height: 100%;
+            min-width: 100%;
+            border-radius: 20px 0 0 20px;
+            object-fit: cover;
+            object-position: 50% 50%;
+            filter: brightness(70%);
+            transition: filter 0.5s ease-in-out;
+
+            &:hover {
+                filter: none;
+            }
+        }
+    `;
+    const TextContainer = styled.div`
+        flex: 0 1 50%;
+        background-color: #ffffff;
+        border-radius: 0 20px 20px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 1rem;
+        text-align: center;
+        font-size: 12px;
+    `;
+
     return (
-        <div>
-            <h1>downloads content!</h1>
+        <div className='container'>
+            <CardContainer>
+                <ImageContainer>
+                    <img
+                        src='https://image.freepik.com/vector-gratis/fondo-ilustracion-criaturas-coloridas-espeluznantes_516247-10.jpg'
+                        alt='foto-random'
+                    />
+                </ImageContainer>
+                <TextContainer>
+                    <h2>holac como estas</h2>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi vel quaerat
+                        nam harum dolorum similique debitis, numquam aperiam dolores eligendi!
+                    </p>
+                    <Button mutual>Descargar</Button>
+                </TextContainer>
+            </CardContainer>
+            <CardContainer>
+                <ImageContainer>
+                    <img
+                        src='https://cdn.pixabay.com/photo/2021/10/02/18/49/square-6676128_960_720.jpg'
+                        alt='foto-random2'
+                    />
+                </ImageContainer>
+                <TextContainer>
+                    <h2>holac como estas</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni error
+                        deleniti possimus dolorum adipisci fugit qui quas, repudiandae maxime
+                        voluptates placeat vel quaerat quae quasi saepe quod vitae perferendis
+                        excepturi. Eligendi at mollitia doloremque. Omnis distinctio amet numquam
+                        quos? Neque.
+                    </p>
+                    <Button mutual>Descargar</Button>
+                </TextContainer>
+            </CardContainer>
         </div>
     );
 };
---
+++ src/components/utils/Button.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+// buttton options:
+// mutual button = green background and with color Font, ex: <button mutual></button>
+// white button(default value) = white background with white font color. ex:<button mutual></button>
+//FontSize as props or || 12 px as default
+export const Button = styled.button`
+    padding: 0.5rem 1rem;
+    font-size: ${({ fontSize }) => fontSize};
+    margin: 0.3rem;
+    border-radius: 20px;
+    border: none;
+    background-color: ${({ mutual }) => mutual && 'var(--mutual-color)'};
+    transition: background-color 0.5s ease, border 0.5s ease;
+    color: ${({ mutual }) => mutual && '#fff'};
+    border: 1px solid var(--mutual-color);
+    cursor: pointer;
+    text-align: center;
+
+    &:hover {
+        background-color: ${({ mutual }) => mutual && 'var(--mutual-color-hover)'};
+        border: 1px solid var(--mutual-color-hover);
+    }
+`;
+
+Button.defaultProps = {
+    fontSize: '12px',
+    color: '#5E5E5E',
+};
