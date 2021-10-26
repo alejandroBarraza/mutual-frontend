0fb5ea87a16d41da55eda9d7d626ebb21b4a748f
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-10-26 15:09:34 -0300

    add route unique new by id

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,12 +4,14 @@ import { Downloads } from '../components/download/Downloads';
 import { Home } from '../components/home/Home';
 import { Navbar } from '../components/home/navbar/Navbar';
 import { New } from '../components/news/New';
+import { NewsById } from '../components/news/NewsById';
 
 export const AppRouter = () => {
     return (
         <Router>
             <Navbar />
             <Switch>
+                <Route exact path='/noticias/:id' component={NewsById} />
                 <Route exact path='/noticias' component={New} />
                 <Route exact path='/descargables' component={Downloads} />
                 <Route path='/' component={Home} />
