4c1556736508396681e379761a80890892e4fc62
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-10-26 18:22:46 -0300

    graphql fetchmore working

--- src/components/news/CardNewsView.js
+++ src/components/news/CardNewsView.js
@@ -9,29 +9,61 @@ import { ErrorUI } from '../utils/ErrorUI';
 import { GETNEWS_START_LIMIT } from '../../Graphql/Queries';
 
 export const CardNewsView = () => {
-    const handleFetchMore = () => {
-        console.log('fetch more');
-    };
+    // const handleFetchMore = () => {
+    //     fetchMore({
+    //         variables: {
+    //             start: data.news[data.news.length - 1].id,
+    //             limit: 3,
+    //         },
+    //         updateQuery: (pv, { fetchMoreResult }) => {
+    //             console.log('pase');
+    //             if (!fetchMoreResult) return pv;
+    //             return {
+    //                 news: [...pv, ...fetchMoreResult.news],
+    //             };
+    //         },
+    //     });
+    // };
 
-    const { data, loading, error } = useQuery(GETNEWS_START_LIMIT, {
-        variables: { start: 0, limit: 5 },
+    const { data, loading, error, fetchMore } = useQuery(GETNEWS_START_LIMIT, {
+        variables: { start: 0, limit: 3 },
     });
 
     if (loading) return <Loading />;
     if (error) return <ErrorUI error={error.message} />;
+    console.log(data.news.length);
 
     return (
         <div className='container'>
             <Grid container spacing={6} alignItems='stretch'>
                 {data.news.map((newData) => (
-                    <Grid item xs={12} sm={6} md={4} sx={{ align: 'center' }}>
+                    <Grid key={newData.id} item xs={12} sm={6} md={4} sx={{ align: 'center' }}>
                         <CardNews key={newData.id} data={newData} maxWidth={'345'} />
                     </Grid>
                 ))}
             </Grid>
 
             <Box textAlign='center' m={8}>
-                <Button variant='outlined' color='success' onClick={handleFetchMore}>
+                <Button
+                    variant='outlined'
+                    color='success'
+                    onClick={() => {
+                        fetchMore({
+                            variables: {
+                                start: data.news.length,
+                                limit: 3,
+                            },
+                            updateQuery: (pv, { fetchMoreResult }) => {
+                                if (!fetchMoreResult) {
+                                    return pv;
+                                }
+
+                                return {
+                                    news: [...pv.news, ...fetchMoreResult.news],
+                                };
+                            },
+                        });
+                    }}>
                     Ver mas
                 </Button>
             </Box>
