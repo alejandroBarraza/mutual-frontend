270afad94399ce7ac323ddc5a6e17702cf3a8a9a
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-11-02 19:01:43 -0300

    download component table

--- src/components/download/Downloads.js
+++ src/components/download/Downloads.js
@@ -1,88 +1,109 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
+import { Box } from '@mui/material';
+import MaterialTable from '@material-table/core';
 
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
-
-const CustomCss = {
-    card: { maxWidth: 345, margin: 'auto' },
-    imagenCustom: {
-        filter: 'brightness(70%)',
-        transition: 'filter 0.5s ease-in-out',
-        '&:hover': {
-            filter: 'none',
-        },
-    },
-};
+// let theme = createTheme({
+//     typography: {
+//         fontFamily: 'Public Sans',
+//         allVariants: {
+//             color: '(var--paragraph-color)',
+//         },
+//     },
+// });
 
 export const Downloads = () => {
-    const [expanded, setExpanded] = React.useState(false);
+    // const { data, loading, error } = useQuery();
 
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
+    // if (loading) return <Loading />;
+    // if (error) return <ErrorUI error={error.message} />;
+    const columns = [
+        { title: 'Titulo', field: 'title' },
+        { title: 'Descripcion', field: 'description' },
+    ];
+    const data = [
+        {
+            title: 'prevencion accidentes',
+            description: 'Lorem, ipsum dolor sit amet ',
+        },
+        {
+            title: 'prevencion accidentes',
+            description: 'Lorem, ipsum dolor sit amet .',
+        },
+        {
+            title: 'a',
+            description: 'Lorem, ipsum dolor sit amet .',
+        },
+        {
+            title: 'prevencion accidentes',
+            description: 'Lorem, ipsum dolor sit amet .',
+        },
+        {
+            title: 'prevencion accidentes',
+            description: 'Lorem, ipsum dolor sit amet .',
+        },
+        {
+            title: 'prevencion accidentes',
+            description: 'Lorem, ipsum dolor sit amet .',
+        },
+        {
+            title: 'prevencion accidentes',
+            description: 'Lorem, ipsum dolor sit amet .',
+        },
+    ];
 
     return (
+        // <ThemeProvider theme={theme}>
         <div className='container'>
-            <Card sx={CustomCss.card}>
-                <CardMedia
-                    component='img'
-                    height='194'
-                    image='https://image.freepik.com/vector-gratis/fondo-halloween-plano-dibujado-mano_23-2149109803.jpg'
-                    alt='random imagen'
-                    sx={CustomCss.imagenCustom}
+            <Box sx={{ mt: 6, borderRadius: '2rem' }}>
+                <MaterialTable
+                    columns={columns}
+                    data={data}
+                    actions={[
+                        {
+                            icon: 'download',
+                            tooltip: 'descargar',
+                            onClick: (event, rowData) => alert('You saved ' + rowData.name),
+                        },
+                    ]}
+                    options={{
+                        actionsColumnIndex: -1,
+                        showTitle: false,
+                        search: true,
+                        searchFieldAlignment: 'left',
+                        headerStyle: {
+                            backgroundColor: ' #F4F6F8',
+                            paddingTop: '1rem',
+                            paddingBottom: '1rem',
+                            color: '#637381',
+                        },
+                        editCellStyle: { borderRadius: '2rem' },
+                    }}
+                    localization={{
+                        header: {
+                            actions: '',
+                        },
+
+                        body: {
+                            emptyDataSourceMessage: 'No hay datos',
+                        },
+                        toolbar: {
+                            searchTooltip: 'Buscar',
+                            searchPlaceholder: 'Buscar',
+                        },
+                        pagination: {
+                            labelRowsSelect: 'Filas',
+                            labelDisplayedRows: '{from}-{to} de {count}',
+                            firstTooltip: 'Primera página',
+                            previousTooltip: 'Página anterior',
+                            nextTooltip: 'Página siguiente',
+                            lastTooltip: 'Última página',
+                            labelRowsPerPage: 'Filas por página:',
+                        },
+                    }}
                 />
-                <CardContent>
-                    <Typography variant='body2' color='text.secondary'>
-                        This impressive paella is a perfect party dish and a fun meal to cook
-                        together with your guests. Add 1 cup of frozen peas along with the mussels,
-                        if you like.
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <ExpandMore
-                        expand={expanded}
-                        onClick={handleExpandClick}
-                        aria-expanded={expanded}
-                        aria-label='show more'>
-                        <ExpandMoreIcon />
-                    </ExpandMore>
-                </CardActions>
-                <Collapse in={expanded} timeout='auto' unmountOnExit>
-                    <CardContent>
-                        <Typography paragraph>Method:</Typography>
-                        <Typography paragraph>
-                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                            aside for 10 minutes.
-                        </Typography>
-                        <Typography paragraph>
-                            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                        </Typography>
-                        <Typography paragraph>
-                            Add rice and stir very gently to distribute. Top with artichokes and p
-                        </Typography>
-                        <Typography>
-                            Set aside off of the heat to let rest for 10 minutes, and then serve.
-                        </Typography>
-                    </CardContent>
-                </Collapse>
-            </Card>
+            </Box>
         </div>
+        // </ThemeProvider>
     );
 };
