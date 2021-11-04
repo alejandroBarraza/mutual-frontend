a50f101fe9dd3ac77f7a2b69940186dace42c2e3
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-11-04 12:56:55 -0300

    order import

--- src/components/download/Downloads.js
+++ src/components/download/Downloads.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Box, Paper, Typography } from '@mui/material';
 import MaterialTable from '@material-table/core';
-import { GETDOWNLOADS_SORTED_DESC } from '../../Graphql/Queries';
 import { useQuery } from '@apollo/client';
-import { Loading } from '../utils/Loading';
-import { ErrorUI } from '../utils/ErrorUI';
+import { Box, Paper, Typography } from '@mui/material';
 import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
+
+import { ErrorUI } from '../utils/ErrorUI';
+import { GETDOWNLOADS_SORTED_DESC } from '../../Graphql/Queries';
+import { Loading } from '../utils/Loading';
+
 export const Downloads = () => {
     const { data, loading, error } = useQuery(GETDOWNLOADS_SORTED_DESC);
     if (loading) return <Loading />;
