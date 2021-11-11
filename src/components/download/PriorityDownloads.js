c15da726f28737123746907407e864f2de763f52
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-11-11 13:26:17 -0300

    fix quick error : key props and load &error state

--- src/components/download/PriorityDownloads.js
+++ src/components/download/PriorityDownloads.js
@@ -1,24 +1,31 @@
 import { useQuery } from '@apollo/client';
 import { Skeleton } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react'
+import React from 'react';
 import { GET_PRIORITY_DOWNLOADS } from '../../Graphql/Queries';
 import { ActionAreaCard } from '../news/ActionAreaCard';
+import { ErrorUI } from '../utils/ErrorUI';
+import { Loading } from '../utils/Loading';
 
 export const PriorityDownloads = () => {
-   const { loading, error, data } = useQuery(GET_PRIORITY_DOWNLOADS);
+    const { loading, error, data } = useQuery(GET_PRIORITY_DOWNLOADS);
+    if (loading) return <Loading />;
+    if (error) return <ErrorUI error={error.message} />;
+    if (!data.downloads.length) return null;
 
-
-   return (
-      <Box sx={{ width: '100%' }}>
-         {
-            (data && data.downloads.lengh !== 0) ? data.downloads.map((element) => {
-               return (
-                     <Box sx={{ width: '100%', py: '.3rem' }} >
-                        <ActionAreaCard download={element} />
-                     </Box>)
-            }) : <Skeleton variant="rectangular" width={'100%'} height={'100%'} />
-         }
-      </Box>
-   );
-}
+    return (
+        <Box sx={{ width: '100%' }}>
+            {data && data.downloads.lengh !== 0 ? (
+                data.downloads.map((element) => {
+                    return (
+                        <Box sx={{ width: '100%', py: '.3rem' }} key={element.id}>
+                            <ActionAreaCard download={element} />
+                        </Box>
+                    );
+                })
+            ) : (
+                <Skeleton variant='rectangular' width={'100%'} height={'100%'} />
+            )}
+        </Box>
+    );
+};
--- src/components/news/NewsById.js
+++ src/components/news/NewsById.js
@@ -14,142 +14,159 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { styles } from './StyleCardNewsView';
 import { CardNews } from './CardNews';
 import { PriorityDownloads } from '../download/PriorityDownloads';
-import { height } from '@mui/system';
 
 const converter = new showdown.Converter();
 
 export const NewsById = () => {
-   const { id } = useParams();
-   /**
-    * Database query.
-    * @param loading indicates if data are still being downloaded.
-    * @param error indicates if any error occurred.
-    * @param data the data returned by the database.
-    */
-   const { loading: loadingData, error: errorData, data } = useQuery(GETNEWSBYID(id));
-   const { data: dataNews, loading, error } = useQuery(GETNEWS_START_LIMIT, {
-      variables: { start: 0, limit: 6 }
-   });
-   const [title, setTitle] = useState('');
-   const [presentationImage, setPresentationImage] = useState('#');
-   const [data_procesed, setData_procesed] = useState([]);
-   const matches = useMediaQuery('(min-width:60rem)');
+    const { id } = useParams();
+    /**
+     * Database query.
+     * @param loading indicates if data are still being downloaded.
+     * @param error indicates if any error occurred.
+     * @param data the data returned by the database.
+     */
+    const { loading: loadingData, error: errorData, data } = useQuery(GETNEWSBYID(id));
+    const {
+        data: dataNews,
+        loading,
+        error,
+    } = useQuery(GETNEWS_START_LIMIT, {
+        variables: { start: 0, limit: 6 },
+    });
+    const [title, setTitle] = useState('');
+    const [presentationImage, setPresentationImage] = useState('#');
+    const [data_procesed, setData_procesed] = useState([]);
+    const matches = useMediaQuery('(min-width:60rem)');
 
-   useEffect(() => {
-      setPresentationImage('#');
-      if (data) {
-         console.log(data);
-         if (data.new != null) {
-            let data_procesing = [];
-            let html = converter.makeHtml(data.new.cuerpo);
-            if (data.new.titulo != null) {
-               setTitle(data.new.titulo);
+    useEffect(() => {
+        setPresentationImage('#');
+        if (data) {
+            if (data.new != null) {
+                let data_procesing = [];
+                let html = converter.makeHtml(data.new.cuerpo);
+                if (data.new.titulo != null) {
+                    setTitle(data.new.titulo);
+                }
+                if (data.new.imagenDePresentacion != null) {
+                    setPresentationImage(data.new.imagenDePresentacion.url);
+                }
+
+                html = html.split('<p>').filter((text) => {
+                    return text !== '';
+                });
+                html.forEach((element) => {
+                    element = element.split('</p>').filter((text) => {
+                        return text !== '' && text !== '\n';
+                    });
+                    data_procesing = [...data_procesing, ...element];
+                });
+
+                setData_procesed([...html]);
             }
-            if (data.new.imagenDePresentacion != null) {
-               setPresentationImage(data.new.imagenDePresentacion.url);
-               console.log(html);
-            }
+        }
+    }, [data]);
 
-            html = html.split('<p>').filter((text) => { return text !== '' });
-            html.forEach(element => {
-               element = element
-                  .split('</p>')
-                  .filter((text) => {
-                     return text !== '' && text !== '\n'
-                  })
-               data_procesing = [
-                  ...data_procesing,
-                  ...element]
-            });
+    return (
+        <Box
+            sx={{
+                minHeight: '72vh',
+                width: '100%',
+                position: 'relative',
+                pb: '2rem',
+            }}>
+            <Box
+                sx={{
+                    bgcolor: '#C3D600',
+                    height: presentationImage !== '#' ? '18rem' : '15rem',
+                    width: '100%',
+                    position: 'absolute',
+                }}></Box>
+            <Container sx={{ position: 'relative' }}>
+                {title ? (
+                    <Typography component='div'>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textAlign: 'center',
+                                height: presentationImage !== '#' ? '10rem' : '15rem',
+                                color: '#fff',
+                                fontWeight: 'bold',
+                                fontSize: '3rem',
+                            }}>
+                            {title}
+                        </Box>
+                    </Typography>
+                ) : (
+                    <Skeleton variant='rectangular' width={'100%'} height={'15rem'} />
+                )}
+            </Container>
+            <Container
+                fixed
+                sx={{
+                    position: 'relative',
+                    py: presentationImage !== '#' ? '0rem' : '2rem',
+                }}>
+                {presentationImage !== '#' ? (
+                    <Box
+                        sx={{
+                            maxHeight: '20rem',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            pb: '2rem',
+                        }}>
+                        <img src={presentationImage} alt='Imagen de Presentacion' />
+                    </Box>
+                ) : null}
+                <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                    <Box sx={{ width: matches ? '80%' : '100%' }}>
+                        {data_procesed ? (
+                            data_procesed.map((element, index) => {
+                                return (
+                                    <Box
+                                        key={index}
+                                        sx={{
+                                            textAlign: element.includes('src') ? 'center' : 'left',
+                                            position: 'relative',
+                                        }}>
+                                        <Markup content={element} />
+                                    </Box>
+                                );
+                            })
+                        ) : (
+                            <Skeleton variant='rectangular' width={'100%'} height={'100%'} />
+                        )}
+                    </Box>
+                    <Box sx={{ width: matches ? '20%' : '100%', pl: '2rem' }}>
+                        <PriorityDownloads />
+                    </Box>
+                </Box>
+                <Box textAling='left' sx={styles.titleContainerNews}>
+                    <Typography variant='h6'>Ultimas Noticias</Typography>
+                </Box>
 
-            setData_procesed([...html]);
-         }
-      }
-   }, [data]);
-
-   return (
-      <Box sx={{
-         minHeight: '72vh',
-         width: '100%',
-         position: 'relative',
-         pb: '2rem'
-      }} >
-         <Box sx={{
-            bgcolor: '#C3D600',
-            height: presentationImage !== '#' ? '18rem' : '15rem',
-            width: '100%',
-            position: 'absolute'
-         }}>
-         </Box>
-         <Container sx={{ position: 'relative' }}>
-            {
-               title ? (
-                  <Typography component="div">
-                     <Box sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        textAlign: 'center',
-                        height: presentationImage !== '#' ? '10rem' : '15rem',
-                        color: '#fff',
-                        fontWeight: 'bold',
-                        fontSize: '3rem',
-                     }}>{title}</Box>
-                  </Typography>) :
-                  <Skeleton variant="rectangular" width={'100%'} height={'15rem'} />
-            }
-         </Container >
-         <Container fixed sx={{
-            position: 'relative',
-            py: presentationImage !== '#' ? '0rem' : '2rem',
-         }}>
-            {
-               presentationImage !== '#' ? <Box sx={{
-                  maxHeight: '20rem',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  pb: '2rem',
-               }}><img src={presentationImage} alt='Imagen de Presentacion' /></Box> : null
-            }
-            <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', }}>
-               <Box sx={{ width: matches ? '80%' : '100%', }}>
-                  {
-                     data_procesed ?
-                        data_procesed.map((element) => {
-                           return (<Box sx={{
-                              textAlign: element.includes('src') ? 'center' : 'left',
-                              position: 'relative',
-                           }}> <Markup content={element} /></Box>)
-                        }) :
-                        <Skeleton variant="rectangular" width={'100%'} height={'100%'} />
-                  }
-               </Box>
-               <Box sx={{ width: matches ? '20%' : '100%', pl: '2rem' }}>
-                  <PriorityDownloads />
-               </Box>
-            </Box>
-            <Box textAling='left' sx={styles.titleContainerNews}>
-               <Typography variant='h6'>Ultimas Noticias</Typography>
-            </Box>
-
-            <Grid container spacing={2} alignItems='stretch'>
-               {
-                  dataNews ? dataNews.news.map((newData) => (
-                     <Grid key={newData.id} item xs={12} sm={6} md={4} sx={{
-                        align: 'center',
-                        height: '25rem'
-                     }}>
-                        <CardNews
-                           descriptionInfo
-                           key={newData.id}
-                           data={newData}
-                        />
-                     </Grid>
-                  )
-                  ) : <Skeleton variant="rectangular" width={'100%'} height={'100%'} />
-               }
-            </Grid>
-         </Container >
-      </Box >
-   )
-}
+                <Grid container spacing={2} alignItems='stretch'>
+                    {dataNews ? (
+                        dataNews.news.map((newData) => (
+                            <Grid
+                                key={newData.id}
+                                item
+                                xs={12}
+                                sm={6}
+                                md={4}
+                                sx={{
+                                    align: 'center',
+                                    height: '26rem',
+                                }}>
+                                <CardNews descriptionInfo key={newData.id} data={newData} />
+                            </Grid>
+                        ))
+                    ) : (
+                        <Skeleton variant='rectangular' width={'100%'} height={'100%'} />
+                    )}
+                </Grid>
+            </Container>
+        </Box>
+    );
+};
