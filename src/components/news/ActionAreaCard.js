de693b31d7830c2e0f9c317aaabb68a72b196d9d
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-11-11 11:52:43 -0300

    quick fix

--- src/components/news/ActionAreaCard.js
+++ src/components/news/ActionAreaCard.js
@@ -4,23 +4,19 @@ import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 
 export const ActionAreaCard = ({ download }) => {
-   const handleDownload = () => {
-      window.open(
-         download.archivo.url,
-         '_blank',
-         'noopener,resizable,scrollbars'
-      )
-   }
-   return (
-      <Card sx={{ width: '100%' }} onClick={handleDownload}>
-         <CardActionArea>
-            <CardMedia
-               component="img"
-               height="140"
-               image={`${download.imagen.url}`}
-               alt="hola"
-            />
-         </CardActionArea>
-      </Card>
-   );
-}
+    const handleDownload = () => {
+        window.open(download.archivo.url, '_blank', 'noopener,resizable,scrollbars');
+    };
+    return (
+        <Card sx={{ width: '100%' }} onClick={handleDownload}>
+            <CardActionArea>
+                <CardMedia
+                    component='img'
+                    height='140'
+                    image={`${download.imagen?.url}`}
+                    alt='hola'
+                />
+            </CardActionArea>
+        </Card>
+    );
+};
