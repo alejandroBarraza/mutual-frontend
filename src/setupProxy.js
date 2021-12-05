6db7cd5696fed33aff55b5eda8928a2a91cf6baa
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-12-05 00:13:31 -0300

    remove proxy

--- src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function (app) {
-    app.use(
-        createProxyMiddleware('/admin', {
-            target: 'http://localhost:1337/admin',
-        })
-    );
-};
