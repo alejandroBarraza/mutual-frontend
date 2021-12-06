cc74980eb9ba1fd407ff4de3ce029cf6ca71b52b
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-12-06 13:11:21 -0300

    framer motion animations

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,13 +3,41 @@ import { CardNewsView } from '../news/CardNewsView';
 import { Statistics } from '../statistics/Statistics';
 import { Video } from '../videos/Video';
 import { ImagesCarousel } from './imagesCarousel/ImagesCarousel';
-
+import {useInView} from 'react-intersection-observer';
+import{useAnimation,motion} from 'framer-motion';
 export const Home = () => {
+
+    const {ref, inView} = useInView();
+    const animation = useAnimation();
+
+    React.useEffect(() => {
+        if(inView){
+            console.log('inView');
+            animation.start({
+                x:0,
+                opacity: 1,
+
+                transition: {
+                    type: 'linear',duration: 1,bounce:1,
+                }
+            })
+        }
+        if(!inView){
+            animation.start({opacity:0,x:'-100vw'})
+        }
+    
+    
+    },[inView,animation])
     return (
         <>
             <ImagesCarousel />
             <Video />
-            <Statistics />
+            <div ref={ref} >
+                <motion.div animate={animation}>
+                    <Statistics/>
+                </motion.div>
+            </div>
+            
             <div id='news'><CardNewsView /></div>
         </>
     );
--- src/components/videos/Video.js
+++ src/components/videos/Video.js
@@ -1,23 +1,53 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 
 import { Query } from '../utils/Query';
 import { YoutubeEmbed } from './YoutubeEmbed';
 import { GETVIDEOS } from '../../Graphql/Queries';
 
-
 import './Video.css';
 import '../../container.css';
 
+// import {useInView} from 'react-intersection-observer';
+// import{useAnimation,motion} from 'framer-motion';
+
 export const Video = () => {
+
+    // const {ref, inView} = useInView();
+    // const animation = useAnimation();
+
+    // useEffect(() => {
+    //     if(inView){
+    //         console.log('inView');
+    //         animation.start({
+    //             opacity: 1,
+    //             y:0,
+
+    //             transition: {
+    //                 type: 'linear',duration: 1,bounce:1,
+    //             }
+    //         })
+    //     }
+    //     if(!inView){
+    //         animation.start({opacity:0,y:-100})
+    //     }
+    
+    
+    // },[inView,animation])
+
+
     return (
         <Query query={GETVIDEOS} id={null}>
             {({ data: { videos } }) => {
                 if (!videos.length) return null;
                 return (
                     <div className='container'>
-                        <h2 className='title-video'>{videos[0]?.titulo}</h2>
-                        <p className='description-video'>{videos[0]?.descripcion}</p>
-                        <YoutubeEmbed embedId={videos[0]?.links} />
+                        {/* <motion.div animate={animation}> */}
+                            <h2 className='title-video'>{videos[0]?.titulo}</h2>
+                            <p className='description-video'>{videos[0]?.descripcion}</p>
+                            <YoutubeEmbed embedId={videos[0]?.links} />
+
+                        {/* </motion.div> */}
+                       
                     </div>
                 );
             }}
