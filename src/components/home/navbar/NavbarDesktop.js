33a9b28c272b29e6958c0164849a6c906387b3fe
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-12-08 13:18:03 -0300

    change url

--- src/components/home/navbar/NavbarDesktop.js
+++ src/components/home/navbar/NavbarDesktop.js
@@ -3,12 +3,10 @@ import { Link } from 'react-router-dom';
 import { NavbarItem } from './NavbarItem';
 import './NavbarDesktop.css';
 import '../../../container.css';
-import { Link as Lonk } from "react-scroll";
+import { Link as Lonk } from 'react-scroll';
 
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { Tooltip, IconButton, Link as LinkMui } from '@mui/material';
-
-
 
 export const NavbarbarDesktop = () => {
     return (
@@ -18,33 +16,38 @@ export const NavbarbarDesktop = () => {
                     <img className='image' src='/mutual-svg.svg' alt='logo-mutual' />
                 </Link>
                 <ul className='navbar-items'>
-                    {NavbarItem.map((item, index) => (
-                        item.section ?
+                    {NavbarItem.map((item, index) =>
+                        item.section ? (
                             <li key={index}>
                                 <Lonk
-                                    activeClass="active"
+                                    activeClass='active'
                                     to={item.section}
                                     spy={true}
                                     smooth={true}
                                     offset={-70}
-                                    duration={500}>
+                                    duration={500}
+                                >
                                     <Link className={item.cName} to={item.url}>
                                         {item.title}
                                     </Link>
                                 </Lonk>
-
                             </li>
-                            :
+                        ) : (
                             <li key={index}>
                                 <Link className={item.cName} to={item.url}>
                                     {item.title}
                                 </Link>
                             </li>
-                    ))}
+                        )
+                    )}
                 </ul>
-                <Tooltip title="Iniciar Sección">
+                <Tooltip title='Iniciar Sección'>
                     <IconButton>
-                        <LinkMui href='https://aunteespero-backend.herokuapp.com/admin' target='_blank' rel='noopener'>
+                        <LinkMui
+                            href='https://aunteespero-admin.herokuapp.com/admin/'
+                            target='_blank'
+                            rel='noopener'
+                        >
                             <AccountCircleIcon sx={{ color: '#91B722' }} />
                         </LinkMui>
                     </IconButton>
