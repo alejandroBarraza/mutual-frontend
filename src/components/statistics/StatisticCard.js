43d099feb791110ee7d4d12fcf1308e852383f4c
alejandroBarraza <alejandro.barraza@alumnos.ucn.cl>
2021-12-07 22:35:04 -0300

    fix empty card database bug

--- src/components/statistics/StatisticCard.js
+++ src/components/statistics/StatisticCard.js
@@ -4,42 +4,53 @@ import { Query } from '../utils/Query';
 import './StatisticCards.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-    faHandHoldingHeart,
-    faHandshake,
-    faStar,
-    faUserPlus,
+  faHandHoldingHeart,
+  faHandshake,
+  faStar,
+  faUserPlus,
 } from '@fortawesome/free-solid-svg-icons';
 
 export const StatisticCard = () => {
-    return (
-        <Query query={GETSTATISTICS} id={null}>
-            {({ data: { statistics } }) => {
-                if (!statistics.length) return null;
-                return (
-                    <>
-                        <div className='card'>
-                            <FontAwesomeIcon icon={faHandHoldingHeart} className='icon-style' />
-                            <h2>{statistics[0].titulo}</h2>
-                            <p>{statistics[0].descripcion}</p>
-                        </div>
-                        <div className='card'>
-                            <FontAwesomeIcon icon={faHandshake} className='icon-style' />
-                            <h2>{statistics[1].titulo}</h2>
-                            <p>{statistics[1].descripcion}</p>
-                        </div>
-                        <div className='card'>
-                            <FontAwesomeIcon icon={faStar} className='icon-style' />
-                            <h2>{statistics[2].titulo}</h2>
-                            <p>{statistics[2].descripcion}</p>
-                        </div>
-                        <div className='card'>
-                            <FontAwesomeIcon icon={faUserPlus} className='icon-style' />
-                            <h2>{statistics[3].titulo}</h2>
-                            <p>{statistics[3].descripcion}</p>
-                        </div>
-                    </>
-                );
-            }}
-        </Query>
-    );
+  return (
+    <Query query={GETSTATISTICS} id={null}>
+      {({ data: { statistics } }) => {
+        if (!statistics.length) return null;
+        return (
+          <>
+            {statistics[0] && (
+              <div className='card'>
+                <FontAwesomeIcon icon={faHandHoldingHeart} className='icon-style' />
+                <h2>{statistics[0]?.titulo}</h2>
+                <p>{statistics[0]?.descripcion}</p>
+              </div>
+            )}
+
+            {statistics[1] && (
+              <div className='card'>
+                <FontAwesomeIcon icon={faHandshake} className='icon-style' />
+                <h2>{statistics[1]?.titulo}</h2>
+                <p>{statistics[1]?.descripcion}</p>
+              </div>
+            )}
+
+            {statistics[2] && (
+              <div className='card'>
+                <FontAwesomeIcon icon={faStar} className='icon-style' />
+                <h2>{statistics[2]?.titulo}</h2>
+                <p>{statistics[2]?.descripcion}</p>
+              </div>
+            )}
+
+            {statistics[3] && (
+              <div className='card'>
+                <FontAwesomeIcon icon={faUserPlus} className='icon-style' />
+                <h2>{statistics[3]?.titulo}</h2>
+                <p>{statistics[3]?.descripcion}</p>
+              </div>
+            )}
+          </>
+        );
+      }}
+    </Query>
+  );
 };
